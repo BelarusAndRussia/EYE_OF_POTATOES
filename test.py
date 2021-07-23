ec4612cd4a6d0ce1446c48f8abca72d44548f44b
BelarusAndRussia <73180653+BelarusAndRussia@users.noreply.github.com>
2021-07-23 16:49:09 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,23 @@
-import telebot
-bot = telebot.TeleBot('1872273294:AAFE2PGjCEIo28bh6UowoELtmJ7Qal3VxG0')
+from flask import Flask, request
+import requests
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, f'Я бот. Приятно познакомиться, {message.from_user.first_name}')
+app = Flask(__name__)
 
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.from_user.id, 'Привет!')
-    else:
-        bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
 
-bot.polling(none_stop=True)
+def send_message(chat_id, text):
+    method = "sendMessage"
+    token = "1872273294:AAFE2PGjCEIo28bh6UowoELtmJ7Qal3VxG0"
+    url = f"https://api.telegram.org/bot{token}/{method}"
+    data = {"chat_id": chat_id, "text": text}
+    requests.post(url, data=data)
+
+
+@app.route("/", methods=["POST"])
+def receive_update():
+    chat_id = request.json["message"]["chat"]["id"]
+    send_message(chat_id, "Hello!")
+    return "ok"
+
+
+if __name__ == "__main__":
+    app.run()
